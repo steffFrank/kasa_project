884e7c2bdfea28e0df43eb6ac6e7d690e303bb01
steffFrank <--replace-all>
2023-01-28 16:37:29 +0100

    feat: Establish the connection to the database

--- backend/app.js
+++ backend/app.js
@@ -11,5 +11,18 @@ app.use(cors());
 mongoose.set("strictQuery", true);
 
 
+try {
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const connection = mongoose.connection;
+    connection.once("open", () => {
+        console.log("MongoDb database connection established successfully!");
+    })
+} catch(error) {
+    console.error(error);
+}
+
 
 export default app;
