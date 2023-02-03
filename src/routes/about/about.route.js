3ff8349a9177861740d2d29bd8c0ad9237ea7dd1
steffFrank <--replace-all>
2023-02-03 16:45:21 +0100

    refactor: Move the texts in about in an object in utils

--- src/routes/about/about.route.js
+++ src/routes/about/about.route.js
@@ -1,5 +1,6 @@
 import styles from "./about.route.module.scss";
 import { Dropdown } from "../../components/dropdown/dropdown.component";
+import { aboutTexts } from "../../utils/about.utils";
 
 export const About = () => {
     return (
@@ -8,18 +9,9 @@ export const About = () => {
                 <div className={styles.darken}></div>
             </main>
             <div className={styles.description}>
-                <Dropdown title="Fiabilité" type="large">
-                    Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
-                </Dropdown>
-                <Dropdown title="Respect" type="large">
-                    La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
-                </Dropdown>
-                <Dropdown title="Service" type="large">
-                    Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
-                </Dropdown>
-                <Dropdown title="Sécurité" type="large">
-                    La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
-                </Dropdown>
+                {aboutTexts.map((text, index) => {
+                    return <Dropdown key={index} type="large" title={text.title}>{text.message}</Dropdown>
+                })}
             </div>
         </section>
     )
