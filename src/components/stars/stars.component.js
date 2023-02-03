717dc6b6b3b9c2d7e80fad202ede46f89e6aa7b5
steffFrank <--replace-all>
2023-02-03 15:50:47 +0100

    refactor: Optimize code

--- src/components/stars/stars.component.js
+++ src/components/stars/stars.component.js
@@ -4,17 +4,17 @@ import { ReactComponent as Star } from "../../assets/images/redStar.svg";
 const NUM_STARS = 5;
 const createStarsArray = (rating) => {
     let stars = [];
-    for (let i = 0; i< NUM_STARS; i++) {
+    for (let i = 0; i < NUM_STARS; i++) {
         if (i < rating) {
-            stars.push(<Star key={i} className={styles.rated}/>)
+            stars.push(<Star key={i} className={styles.rated} />)
         } else {
             stars.push(<Star key={i} className={styles.unrated} />)
         }
     }
     return stars;
 }
-export const Stars = ({rating}) => {
- 
+export const Stars = ({ rating }) => {
+
 
     return (
         <div className={styles.stars}>
