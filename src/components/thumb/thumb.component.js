4cff26d6a03ccb3152a1ca1e335f5a4194726430
steffFrank <--replace-all>
2023-02-05 20:10:24 +0100

    refactor: Move the cover of the thumb in the css

--- src/components/thumb/thumb.component.js
+++ src/components/thumb/thumb.component.js
@@ -4,18 +4,14 @@ import styles from "./thumb.component.module.scss";
 
 export const Thumb = ({ title, id, cover }) => {
 
-    const divStyle = {
-        background: `url(${cover})`,
-        backgroundSize: "cover"
-    };
-
     return (
         <Link className={styles.link} to={id}>
-            <article className={styles.article} style={divStyle}>
-                <div className={styles.darken}>
-                    <h2 className={styles.title}>{title}</h2>
-                </div>
-            </article>
+            <h2 className={styles.title}>{title}</h2>
+            <div className={styles.darken}>
+                <article className={`${styles.article} `}>
+                    <img src={cover} alt={title} className={styles.img} />
+                </article>
+            </div>
         </Link>
     )
 }
